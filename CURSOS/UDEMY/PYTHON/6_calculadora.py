938196edc647ab82681575a7b155bb60688ae303
jeferson-cerqueira <jefersonduarte886@gmail.com>
2025-11-03 20:44:14 -0300

    alteração de numeros

--- CURSOS/UDEMY/PYTHON/6_calculadora.py
+++ CURSOS/UDEMY/PYTHON/6_calculadora.py
@@ -8,13 +8,13 @@ try:
         
         if deseja_iniciar == "S":
 
-            numero_1 = int(input("digite um numero : "))
-            numero_2 = int(input("digite outro numero : "))
+            numero_1 = float(input("digite um numero : "))
+            numero_2 = float(input("digite outro numero : "))
             print("os operadores validos são [+,-,*,/]")
             digite_operadores = input("digite um operador : ")
 
             if digite_operadores == "+": 
-                print(f'a soma é : {numero_1 + numero_2}')
+                print(f'a soma é : {numero_1 + numero_2:.2f}')
             elif digite_operadores == "-": 
                 print(f'a subtração é : {numero_1 - numero_2}')
             elif digite_operadores == "*": 
